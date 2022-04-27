b4f32f3fd60a8ea9a6298561f954e4856f962ff8
Reynan Kanindot <ekahiyang@gmail.com>
2022-04-27 14:09:31 +1000

    image dispaly had gone awry

--- src/components/pages/CodeCard.js
+++ src/components/pages/CodeCard.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react';
 import CodeItems from './CodeItems.js'
-import Image from '../assets/images/react.png'
 import '../assets/css/style.css'
-// import IMAGES from './index2.json'
-
+import image from '../assets/images/mongo-db.jpg'
 
 
 class CodeCard extends Component {
@@ -13,13 +11,13 @@ class CodeCard extends Component {
             <div className='container' style={{ height: "500px" }}>
                 <div className='row row-cols-3 ' style={{ height: "400px" }}>
                     {
-                        CodeItems && CodeItems.map((item, index) => {
+                        CodeItems && CodeItems.map((item) => {
 
                             return (
                                 <div className='col box h-50 text-center'>
                                     <div className='col box h-100 text-center' >
 
-                                        <div key={index} >
+                                        <div key={item.title} >
                                             <a href={item.applink} target="_blank" rel="noreferrer">{item.title}</a>
                                             <a href={item.githublink} target="_blank" rel="noreferrer"><i className="fa-brands fa-github-square fa-2xl px-3"></i></a>
 
--- src/components/pages/CodeItems.js
+++ src/components/pages/CodeItems.js
@@ -1,50 +1,51 @@
 
-const CodeItems = 
-[
-    {
-    
-        title: 'Web-DevOp-Quiz',
-        githublink: 'https://github.com/Eugene32/Web-DevOp-Quiz',
-        applink: 'https://eugene32.github.io/Web-DevOp-Quiz/',
-        image: "/images/jscript-image.jpeg"
+const CodeItems =
+    [
+        {
 
-    },
-    {
-   
-        title: 'Weather Dashboard',
-        githublink: 'https://github.com/Eugene32/Weather-Dashboard',
-        applink: 'https://eugene32.github.io/Weather-Dashboard/',
-        image: "/images/web-api-tutorial.png"
-    },
-    {
-  
-        title: 'Password Generator',
-        githublink: 'https://github.com/Eugene32/Password-Generator',
-        applink: 'https://eugene32.github.io/Password-Generator/',
-        image: "/images/nav-background.jpg"
-    },
-    {
-   
-        title: 'MVC Tech-Blog',
-        githublink: 'https://github.com/Eugene32/MVC-Tech-Blog',
-        applink: 'https://mvc-tech-blog-rey.herokuapp.com/',
-        image: "/images/SQL-image.jpeg"
-    },
-    {
-    
-        title: 'ECommerce Backend',
-        githublink: 'https://github.com/Eugene32/ECommerce-Back-End',
-        applink: 'https://drive.google.com/file/d/1q4l_tl_GvmR4kw3qaUc51PIPfPwQayCi/view',
-        image: "/images/SQL-image.jpeg"
-    },
-    {
- 
-        title: 'Social Network API (Mongodb)',
-        githublink: 'https://github.com/Eugene32/Social-Network-API',
-        applink: 'https://drive.google.com/file/d/1v-e1Yybqx7KxnCljpmiPk92psB3gJTKs/view',
-        image: "/images/mongo-db.jpg"
-    },
+            title: 'Web-DevOp-Quiz',
+            githublink: 'https://github.com/Eugene32/Web-DevOp-Quiz',
+            applink: 'https://eugene32.github.io/Web-DevOp-Quiz/',
+            image: "/images/jscript-image.jpeg",
 
-]
+
+        },
+        {
+
+            title: 'Weather Dashboard',
+            githublink: 'https://github.com/Eugene32/Weather-Dashboard',
+            applink: 'https://eugene32.github.io/Weather-Dashboard/',
+            image: "/images/web-api-tutorial.png"
+        },
+        {
+
+            title: 'Password Generator',
+            githublink: 'https://github.com/Eugene32/Password-Generator',
+            applink: 'https://eugene32.github.io/Password-Generator/',
+            image: "/images/nav-background.jpg"
+        },
+        {
+
+            title: 'MVC Tech-Blog',
+            githublink: 'https://github.com/Eugene32/MVC-Tech-Blog',
+            applink: 'https://mvc-tech-blog-rey.herokuapp.com/',
+            image: "/images/SQL-image.jpeg"
+        },
+        {
+
+            title: 'ECommerce Backend',
+            githublink: 'https://github.com/Eugene32/ECommerce-Back-End',
+            applink: 'https://drive.google.com/file/d/1q4l_tl_GvmR4kw3qaUc51PIPfPwQayCi/view',
+            image: "/images/SQL-image.jpeg"
+        },
+        {
+
+            title: 'Social Network API (Mongodb)',
+            githublink: 'https://github.com/Eugene32/Social-Network-API',
+            applink: 'https://drive.google.com/file/d/1v-e1Yybqx7KxnCljpmiPk92psB3gJTKs/view',
+            image: "/images/mongo-db.jpg"
+        },
+
+    ]
 
 export default CodeItems;
